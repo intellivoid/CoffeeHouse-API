6d1db4d855ea863f4296be29c3b0bfe32fca692e
Netkas <netkas@intellivoid.info>
2021-01-28 12:22:21 -0500

    Updated Subscription update method

--- src/resources/modules/v1/script.check_subscription.php
+++ src/resources/modules/v1/script.check_subscription.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpMissingFieldTypeInspection */
 
     use COASniffle\Exceptions\CoaAuthenticationException;
     use COASniffle\Handlers\COA;
@@ -9,11 +9,11 @@
     use Handler\GenericResponses\InternalServerError;
     use Handler\Handler;
     use IntellivoidAPI\Exceptions\AccessRecordNotFoundException;
+    use IntellivoidAPI\Exceptions\DatabaseException;
     use IntellivoidAPI\Exceptions\InvalidRateLimitConfiguration;
+    use IntellivoidAPI\Exceptions\InvalidSearchMethodException;
     use IntellivoidAPI\Objects\AccessRecord;
     use IntellivoidSubscriptionManager\Abstracts\SearchMethods\SubscriptionSearchMethod;
-    use IntellivoidSubscriptionManager\Exceptions\DatabaseException;
-    use IntellivoidSubscriptionManager\Exceptions\InvalidSearchMethodException;
     use IntellivoidSubscriptionManager\Exceptions\SubscriptionNotFoundException;
     use IntellivoidSubscriptionManager\IntellivoidSubscriptionManager;
     use IntellivoidSubscriptionManager\Objects\Subscription;
@@ -46,25 +46,22 @@
          * Processes the access key and determines if it used against a valid subscription.
          *
          * @param CoffeeHouse $coffeeHouse
-         * @param AccessRecord $accessRecord
+         * @param AccessRecord $access_record
          * @return null|array
          * @throws AccessRecordNotFoundException
-         * @throws DatabaseException
          * @throws InvalidRateLimitConfiguration
+         * @throws DatabaseException
          * @throws InvalidSearchMethodException
-         * @throws SubscriptionNotFoundException
-         * @throws \IntellivoidAPI\Exceptions\DatabaseException
-         * @throws \IntellivoidAPI\Exceptions\InvalidSearchMethodException
          */
-        public function validateUserSubscription(CoffeeHouse $coffeeHouse, AccessRecord $accessRecord): ?array
+        public function validateUserSubscription(CoffeeHouse $coffeeHouse, AccessRecord $access_record): ?array
         {
             try
             {
                 $UserSubscription = $coffeeHouse->getUserSubscriptionManager()->getUserSubscription(
-                    UserSubscriptionSearchMethod::byAccessRecordID, $accessRecord->ID
+                    UserSubscriptionSearchMethod::byAccessRecordID, $access_record->ID
                 );
             }
-            catch (UserSubscriptionNotFoundException $e)
+            catch (UserSubscriptionNotFoundException)
             {
                 return $this::buildResponse(array(
                     "success" => false,
@@ -77,7 +74,7 @@
                     ),
                     500,
                     array(
-                        "access_record" => $accessRecord->toArray()
+                        "access_record" => $access_record->toArray()
                     )
                 );
 
@@ -110,7 +107,7 @@
                     ),
                     403,
                     array(
-                        "access_record" => $accessRecord->toArray(),
+                        "access_record" => $access_record->toArray(),
                         "user_subscription" => $UserSubscription->toArray()
                     )
                 );
@@ -119,6 +116,26 @@
             {
                 InternalServerError::executeResponse($e);
                 exit();
+            }
+
+            // PATCH: Sometimes if a user wants to upgrade their subscription, the features are not yet applied.
+            $Features = Converter::featuresToSA($Subscription->Properties->Features, true);
+
+            if(self::updateSubscriptionFeatures($Features, $access_record) == false)
+            {
+                return $this::buildResponse(array(
+                    "success" => false,
+                    "response_code" => 403,
+                    "error" => array(
+                        "error_code" => 0,
+                        "type" => "SUBSCRIPTION",
+                        "message" => "There are new updates to your subscription, login to the dashboard to update your subscription."
+                    )
+                ), 403, array(
+                        "access_record" => $access_record->toArray(),
+                        "user_subscription" => $UserSubscription->toArray()
+                    )
+                );
             }
 
             if((int)time() > $Subscription->NextBillingCycle)
@@ -135,12 +152,12 @@
                         "success" => false,
                         "response_code" => 500,
                         "error" => array(
-                                "error_code" => $e->getCode(),
+                                "error_code" => 0,
                                 "type" => "SUBSCRIPTION",
                                 "message" => $e->getMessage()
                             )
                         ), 500, array(
-                            "access_record" => $accessRecord->toArray(),
+                            "access_record" => $access_record->toArray(),
                             "user_subscription" => $UserSubscription->toArray()
                         )
                     );
@@ -151,30 +168,173 @@
                     exit();
                 }
 
-                $Subscription = $IntellivoidSubscriptionManager->getSubscriptionManager()->getSubscription(
-                    SubscriptionSearchMethod::byId, $UserSubscription->SubscriptionID
-                );
-
-                // PATCH: Sometimes if a user wants to upgrade their subscription, the features are not yet applied.
-                $Features = Converter::featuresToSA($Subscription->Properties->Features, true);
-
-                if(isset($Features["LYDIA_SESSIONS"]))
-                    $accessRecord->Variables["MAX_LYDIA_SESSIONS"] = (int)$Features["LYDIA_SESSIONS"];
-
                 if($BillingProcessed)
                 {
-                    $accessRecord->Variables["LYDIA_SESSIONS"] = 0;
+                    // Reset all counters
+                    $access_record->Variables["LYDIA_SESSIONS"] = 0;
+                    $access_record->Variables["NFW_CHECKS"] = 0;
+                    $access_record->Variables["POS_CHECKS"] = 0;
+                    $access_record->Variables["SENTIMENT_CHECKS"] = 0;
+                    $access_record->Variables["EMOTION_CHECKS"] = 0;
+                    $access_record->Variables["SPAM_CHECKS"] = 0;
+                    $access_record->Variables["SENTENCE_SPLITS"] = 0;
+                    $access_record->Variables["LANGUAGE_CHECKS"] = 0;
+                    $access_record->Variables["NER_CHECKS"] = 0;
                 }
-
-                $IntellivoidAPI = Handler::getIntellivoidAPI();
-                $IntellivoidAPI->getAccessKeyManager()->updateAccessRecord($accessRecord);
-
             }
+
+            $IntellivoidAPI = Handler::getIntellivoidAPI();
+            $IntellivoidAPI->getAccessKeyManager()->updateAccessRecord($access_record);
 
             $this->subscription = $Subscription;
             $this->user_subscription = $UserSubscription;
 
             return null;
+        }
+
+        /**
+         * Updates all the API Variables from the subscription to make sure it's up to date.
+         *
+         * @param array $features
+         * @param AccessRecord $access_record
+         * @return bool
+         */
+        private static function updateSubscriptionFeatures(array $features, AccessRecord &$access_record): bool
+        {
+            if(isset($features["LYDIA_SESSIONS"]))
+            {
+                $access_record->Variables["MAX_LYDIA_SESSIONS"] = (int)$features["LYDIA_SESSIONS"];
+
+                if(isset($access_record->Variables["LYDIA_SESSIONS"]) == false)
+                    $access_record->Variables["LYDIA_SESSIONS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_NLP_CHARACTERS"]))
+            {
+                $access_record->Variables["MAX_NLP_CHARACTERS"] = (int)$features["MAX_NLP_CHARACTERS"];
+            }
+            else
+            {
+               return false;
+            }
+
+            if(isset($features["MAX_GENERALIZATION_SIZE"]))
+            {
+                $access_record->Variables["MAX_GENERALIZATION_SIZE"] = (int)$features["MAX_GENERALIZATION_SIZE"];
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_NSFW_CHECKS"]))
+            {
+                $access_record->Variables["MAX_NSFW_CHECKS"] = (int)$features["MAX_NSFW_CHECKS"];
+
+                if(isset($access_record->Variables["NFW_CHECKS"]) == false)
+                    $access_record->Variables["NFW_CHECKS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["LIMITED_NAMED_ENTITIES"]))
+            {
+                // True = 8 Named Entities
+                // False = 19 Named Entities
+                $access_record->Variables["LIMITED_NAMED_ENTITIES"] = (bool)$features["LIMITED_NAMED_ENTITIES"];
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_POS_CHECKS"]))
+            {
+                $access_record->Variables["MAX_POS_CHECKS"] = (int)$features["MAX_POS_CHECKS"];
+
+                if(isset($access_record->Variables["POS_CHECKS"]) == false)
+                    $access_record->Variables["POS_CHECKS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_SENTIMENT_CHECKS"]))
+            {
+                $access_record->Variables["MAX_SENTIMENT_CHECKS"] = (int)$features["MAX_SENTIMENT_CHECKS"];
+
+                if(isset($access_record->Variables["SENTIMENT_CHECKS"]) == false)
+                    $access_record->Variables["SENTIMENT_CHECKS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_EMOTION_CHECKS"]))
+            {
+                $access_record->Variables["MAX_EMOTION_CHECKS"] = (int)$features["MAX_EMOTION_CHECKS"];
+
+                if(isset($access_record->Variables["EMOTION_CHECKS"]) == false)
+                    $access_record->Variables["EMOTION_CHECKS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_SPAM_CHECKS"]))
+            {
+                $access_record->Variables["MAX_SPAM_CHECKS"] = (int)$features["MAX_SPAM_CHECKS"];
+                if(isset($access_record->Variables["SPAM_CHECKS"]) == false)
+                    $access_record->Variables["SPAM_CHECKS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_SENTENCE_SPLITS"]))
+            {
+                $access_record->Variables["MAX_SENTENCE_SPLITS"] = (int)$features["MAX_SENTENCE_SPLITS"];
+                if(isset($access_record->Variables["SENTENCE_SPLITS"]) == false)
+                    $access_record->Variables["SENTENCE_SPLITS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_LANGUAGE_CHECKS"]))
+            {
+                $access_record->Variables["MAX_LANGUAGE_CHECKS"] = (int)$features["MAX_LANGUAGE_CHECKS"];
+                if(isset($access_record->Variables["LANGUAGE_CHECKS"]) == false)
+                    $access_record->Variables["LANGUAGE_CHECKS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            if(isset($features["MAX_NER_CHECKS"]))
+            {
+                $access_record->Variables["MAX_NER_CHECKS"] = (int)$features["MAX_NER_CHECKS"];
+                if(isset($access_record->Variables["NER_CHECKS"]) == false)
+                    $access_record->Variables["NER_CHECKS"] = 0;
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /**
@@ -184,6 +344,7 @@
          * @param int $response_code
          * @param array $debugging_info
          * @return array
+         * @noinspection PhpArrayShapeAttributeCanBeAddedInspection
          */
         private static function buildResponse(array $response_content, int $response_code, array $debugging_info): array
         {
