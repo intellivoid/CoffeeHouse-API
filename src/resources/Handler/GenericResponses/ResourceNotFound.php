637d4c2d74ddcf987cd1cd1ad4af0104affaa9be
Netkas <netkas@intellivoid.info>
2021-01-31 23:41:46 -0500

    Added Copyright

--- src/resources/Handler/GenericResponses/ResourceNotFound.php
+++ src/resources/Handler/GenericResponses/ResourceNotFound.php
@@ -1,7 +1,13 @@
 <?php
+    /*
+     * Copyright (c) 2017-2021. Intellivoid Technologies
+     *
+     * All rights reserved, this is a closed-source solution written by Zi Xing Narrakas,
+     *  under no circumstances is any entity with access to this file should redistribute
+     *  without written permission from Intellivoid and or the original Author.
+     */
 
-
-    namespace Handler\GenericResponses;
+namespace Handler\GenericResponses;
 
     /**
      * Class ResourceNotFound
